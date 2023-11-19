8d232bc1d37514a15ad1cd0bad9882acbeb228b5
LFM <146396539+lfm-dev@users.noreply.github.com>
2023-11-19 15:33:34 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while : ; do
+    echo -n "path to your guides: "
+    read pathToGuides
+    if [ -d "$pathToGuides" ]; then
+        break
+    else
+        echo "$pathToGuides doesnt exists, try again."
+    fi
+done
+
+wget https://raw.githubusercontent.com/lfm-dev/helpme/main/helpme.py
+sed -i -e "s#    guides_path = '/path/to/your/guides/folder'#    guides_path = '$pathToGuides'#" helpme.py # add path to script
+
+sudo mv helpme.py /usr/local/bin/helpme
+sudo chmod a+rx /usr/local/bin/helpme
