108e15a1751411399dc295f6bc128ed5332abf9d
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-11 11:31:20 -0300

    refactoring

--- helpme.py
+++ helpme.py
@@ -9,7 +9,10 @@ from rich.markdown import Markdown
 GUIDES_PATH = '/path/to/your/guides/folder'
 
 class Guide:
-    def __init__(self, filename: str, path: str, guides_path: str):
+    '''
+    Class for a markdown how-to guide
+    '''
+    def __init__(self, filename: str, path: str, guides_path: str) -> None:
         self.filename = filename
         self.path = os.path.join(path, self.filename)
         self.partial_path = path.replace(guides_path, '')
@@ -21,32 +24,36 @@ class Guide:
     def get_partial_path_split(self) -> str:
         return self.partial_path.lstrip('/').split('/')
 
-def color_to_comments(guide_text) -> None:
-    guide_text = guide_text.split('\n')
-    code_block = False
-    for line_index, line in enumerate(guide_text):
-        if line.strip().startswith('```'):
-            code_block = not code_block
-        elif code_block: # comments in code are rendered with the specific lexer
-            continue
-        if not line.startswith('#') and '# ' in line:
-            guide_text[line_index] = f'{line[:line.find(" #")]} `{line[line.find(" #")+1:]}`' # so it is recognized as a python inline code block
-    guide_text = ('\n').join(guide_text)
-    return guide_text
+    def get_guide_content(self) -> str:
+        with open(self.path, 'r') as f:
+            guide_text = f.read()
+        guide_text = self.color_to_comments(guide_text)
+        return guide_text
 
-def print_chosen_guide_content(fullpath: str) -> None:
+    def color_to_comments(self, guide_text: str) -> str:
+        guide_text_lines = guide_text.split('\n')
+        code_block = False
+        for line_index, line in enumerate(guide_text_lines):
+            if line.strip().startswith('```'):
+                code_block = not code_block
+            elif code_block: # comments in code are rendered with the specific lexer
+                continue
+            if not line.startswith('#') and '# ' in line:
+                guide_text_lines[line_index] = f'{line[:line.find(" #")]} `{line[line.find(" #")+1:]}`' # so it is recognized as a python inline code block
+        guide_text = ('\n').join(guide_text_lines)
+        return guide_text
+
+def print_chosen_guide_content(guide: Guide) -> None:
     '''
     Prints the content of the .md file with format
     '''
-    with open(fullpath, 'r') as f:
-        file_text = f.read()
-    file_text = color_to_comments(file_text)
+    guide_text = guide.get_guide_content()
     console = Console()
-    md = Markdown(file_text, inline_code_lexer='Python', inline_code_theme='rrt')
+    md = Markdown(guide_text, inline_code_lexer='Python', inline_code_theme='rrt')
     console.print(md)
     print() # empty line at EOF
 
-def print_hits(hits: list) -> None:
+def print_hits(hits: list[Guide]) -> None:
     table = Table(show_header=True, header_style='bold green')
     table.add_column('ID', justify='left')
     table.add_column('Path', justify='left')
@@ -75,7 +82,7 @@ def get_chosen_guide_index(max_guide_index: int) -> int:
             else:
                 print('Wrong ID, try again.')
 
-def get_queries() -> list:
+def get_queries() -> list[str]:
     queries = sys.argv[1:]
     if not queries:
         print_help()
@@ -86,13 +93,13 @@ def print_help() -> None:
     print('       helpme all (shows all guides)')
     sys.exit(1)
 
-def all_queries_in_guide_keywords(queries: list, keywords: list) -> bool:
+def all_queries_in_guide_keywords(queries: list[str], keywords: list[str]) -> bool:
     for query in queries:
         if query.casefold() not in keywords:
             return False
     return True
 
-def get_hits(queries: list) -> list:
+def get_hits(queries: list[str]) -> list[Guide]:
     '''
     Walks by the guides directory and searches for the user queries
     Query can be in the name of the .md file or in the directory name
@@ -119,11 +126,11 @@ def main():
         print('No hits found.')
         sys.exit(0)
     if len(hits) == 1:
-        print_chosen_guide_content(hits[0].path)
+        print_chosen_guide_content(hits[0])
     else:
         print_hits(hits)
         chosen_guide_index = get_chosen_guide_index(max_guide_index = len(hits))
-        print_chosen_guide_content(hits[chosen_guide_index].path)
+        print_chosen_guide_content(hits[chosen_guide_index])
 
 if __name__ == '__main__':
     main()
