63b60ec86b938606024c9f092cb3ebb6b6c36ab7
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-11 11:06:15 -0300

    refactoring

--- helpme.py
+++ helpme.py
@@ -6,6 +6,8 @@ from rich.console import Console
 from rich.markdown import Markdown
 
 
+GUIDES_PATH = '/path/to/your/guides/folder'
+
 class Guide:
     def __init__(self, filename: str, path: str, guides_path: str):
         self.filename = filename
@@ -13,13 +15,13 @@ class Guide:
         self.partial_path = path.replace(guides_path, '')
         self.keywords = self.get_partial_path_split() + self.get_filename_split()
 
-    def get_filename_split(self):
+    def get_filename_split(self) -> str:
         return self.filename[:self.filename.rfind('.')].split('_')
 
-    def get_partial_path_split(self):
+    def get_partial_path_split(self) -> str:
         return self.partial_path.lstrip('/').split('/')
 
-def color_to_comments(guide_text):
+def color_to_comments(guide_text) -> None:
     guide_text = guide_text.split('\n')
     code_block = False
     for line_index, line in enumerate(guide_text):
@@ -28,11 +30,11 @@ def color_to_comments(guide_text):
         elif code_block: # comments in code are rendered with the specific lexer
             continue
         if not line.startswith('#') and '# ' in line:
-            guide_text[line_index] = f'{line[:line.find(" #")]} `{line[line.find(" #")+1:]}`'
+            guide_text[line_index] = f'{line[:line.find(" #")]} `{line[line.find(" #")+1:]}`' # so it is recognized as a python inline code block
     guide_text = ('\n').join(guide_text)
     return guide_text
 
-def print_chosen_guide_content(fullpath: str):
+def print_chosen_guide_content(fullpath: str) -> None:
     '''
     Prints the content of the .md file with format
     '''
@@ -44,7 +46,7 @@ def print_chosen_guide_content(fullpath: str):
     console.print(md)
     print() # empty line at EOF
 
-def print_hits(hits: list):
+def print_hits(hits: list) -> None:
     table = Table(show_header=True, header_style='bold green')
     table.add_column('ID', justify='left')
     table.add_column('Path', justify='left')
@@ -75,11 +77,11 @@ def get_chosen_guide_index(max_guide_index: int) -> int:
 
 def get_queries() -> list:
     queries = sys.argv[1:]
-    if queries:
-        return queries
-    print_help()
+    if not queries:
+        print_help()
+    return queries
 
-def print_help():
+def print_help() -> None:
     print('Usage: helpme query1 query2 ...')
     print('       helpme all (shows all guides)')
     sys.exit(1)
@@ -90,16 +92,16 @@ def all_queries_in_guide_keywords(queries: list, keywords: list) -> bool:
             return False
     return True
 
-def get_hits(queries: list, guides_path: str) -> list:
+def get_hits(queries: list) -> list:
     '''
     Walks by the guides directory and searches for the user queries
     Query can be in the name of the .md file or in the directory name
     Returns a list of Guide objects
     '''
     hits = []
-    for path, _, files in sorted(os.walk(guides_path)):
+    for path, _, files in sorted(os.walk(GUIDES_PATH)):
         for filename in files:
-            guide = Guide(filename, path, guides_path)
+            guide = Guide(filename, path, GUIDES_PATH)
             if 'all' in queries: # all guides are appended
                 hits.append(guide)
                 continue
@@ -109,11 +111,10 @@ def get_hits(queries: list, guides_path: str) -> list:
     return hits
 
 def main():
-    guides_path = '/path/to/your/guides/folder'
-    os.chdir(guides_path)
+    os.chdir(GUIDES_PATH)
     queries = get_queries()
 
-    hits = get_hits(queries, guides_path)
+    hits = get_hits(queries)
     if not hits:
         print('No hits found.')
         sys.exit(0)
