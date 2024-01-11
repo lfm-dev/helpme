706518843761d7b68a80593059851125f5de1016
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-11 13:37:55 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -L "/usr/local/bin/helpme" ]; then
+    sudo rm "/usr/local/bin/helpme"
+fi
+
+if [ -d "/usr/local/bin/helpme_files" ]; then
+    sudo rm -r "/usr/local/bin/helpme_files"
+fi
+
 while : ; do
     echo -n "path to your guides: "
     read pathToGuides
@@ -10,9 +18,11 @@ while : ; do
     fi
 done
 
-wget https://raw.githubusercontent.com/lfm-dev/helpme/main/helpme.py
-sed -i -e "s#    guides_path = '/path/to/your/guides/folder'#    guides_path = '$pathToGuides'#" helpme.py # add path to script
-
-sudo mv helpme.py /usr/local/bin/helpme
-sudo chmod a+rx /usr/local/bin/helpme
-rm install.sh
+wget http://github.com/lfm-dev/helpme/archive/master.zip -O helpme.zip
+unzip helpme.zip
+sed -i -e "s#GUIDES_PATH = '/path/to/your/guides/folder'#GUIDES_PATH = '$pathToGuides'#" helpme-main/src/helpme.py # add path to script
+sudo cp -r helpme-main/src /usr/local/bin/helpme_files
+rm -r helpme-main helpme.zip
+cd /usr/local/bin
+sudo ln -s /usr/local/bin/helpme_files/helpme.py helpme
+sudo chmod a+rx helpme
